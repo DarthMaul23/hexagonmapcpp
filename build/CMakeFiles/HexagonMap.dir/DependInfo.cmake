ac7b248d29de39e479f97a01af921cc6f643ee24
František Moucha <fand.mou@gmail.com>
2025-04-01 15:04:47 +0200

    fix

---
+++ build/CMakeFiles/HexagonMap.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/Army.cpp" "CMakeFiles/HexagonMap.dir/src/Army.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/Army.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/City.cpp" "CMakeFiles/HexagonMap.dir/src/City.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/City.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/CityManager.cpp" "CMakeFiles/HexagonMap.dir/src/CityManager.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/CityManager.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/GameManager.cpp" "CMakeFiles/HexagonMap.dir/src/GameManager.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/GameManager.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/Hero.cpp" "CMakeFiles/HexagonMap.dir/src/Hero.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/Hero.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/NPCMerchant.cpp" "CMakeFiles/HexagonMap.dir/src/NPCMerchant.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/NPCMerchant.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/PathFinder.cpp" "CMakeFiles/HexagonMap.dir/src/PathFinder.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/PathFinder.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/PlayerUnit.cpp" "CMakeFiles/HexagonMap.dir/src/PlayerUnit.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/PlayerUnit.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/Tile.cpp" "CMakeFiles/HexagonMap.dir/src/Tile.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/Tile.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/UIManager.cpp" "CMakeFiles/HexagonMap.dir/src/UIManager.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/UIManager.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/UnitManager.cpp" "CMakeFiles/HexagonMap.dir/src/UnitManager.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/UnitManager.cpp.o.d"
+  "/Users/mp/Documents/CPP/hexagonmapcpp/src/main.cpp" "CMakeFiles/HexagonMap.dir/src/main.cpp.o" "gcc" "CMakeFiles/HexagonMap.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/HexagonMap.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HexagonMap.dir/src/Army.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/Army.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/City.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/City.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/CityManager.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/CityManager.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/GameManager.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/GameManager.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/Hero.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/Hero.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/NPCMerchant.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/NPCMerchant.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/PathFinder.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/PathFinder.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/PlayerUnit.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/PlayerUnit.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/Tile.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/Tile.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/UIManager.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/UIManager.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/UnitManager.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/UnitManager.cpp.o.d"
+  "CMakeFiles/HexagonMap.dir/src/main.cpp.o"
+  "CMakeFiles/HexagonMap.dir/src/main.cpp.o.d"
+  "HexagonMap"
+  "HexagonMap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HexagonMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
